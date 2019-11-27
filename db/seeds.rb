0ec80c0add8e5fbc53352bc58d89a934ca63bf10
Greg Shanks <greg.shanks@gmail.com>
2019-11-27 10:32:27 +0000

    Added save method, tested, data showing in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require_relative('..models/students')
+require_relative('../models/student.rb')
 
 student1 = Student.new({
    "first_name" => "Harry",
@@ -14,5 +14,5 @@ student2 = Student.new({
     "age" => 13
   })
 
-# student1.save
-# student2.save
+student1.save
+student2.save
--- models/student.rb
+++ models/student.rb
@@ -12,8 +12,22 @@ class Student
     @age = details['age'].to_i
   end
 
-  # def save()
-  #
-  # end
+  def save()
+    sql = "INSERT INTO students
+    (
+      first_name,
+      last_name,
+      house,
+      age
+    )
+    VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING *"
+    values = [@first_name, @last_name, @house, @age]
+    student_data = SqlRunner.run(sql, values)
+    @id = student_data.first()['id'].to_i
+  end
 
 end
